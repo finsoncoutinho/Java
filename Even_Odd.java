5d405a49fc9762189c25ca5fdd5371a66939fff6
FINSON COUTINHO <69283994+finsoncoutinho@users.noreply.github.com>
2022-04-02 08:33:46 +0530

    Update Even_Odd.java

--- Even_Odd.java
+++ Even_Odd.java
@@ -1,7 +1,7 @@
 package com.fin;
 import java.util.Scanner;
 
-public class evenodd {
+public class Even_Odd {
     public static void main(String[] args) {
         System.out.print("Enter number: ");
         Scanner sc= new Scanner(System.in);
