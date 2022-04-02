708fddec7f34232970b0dbcd71fee81687fe9574
FINSON COUTINHO <69283994+finsoncoutinho@users.noreply.github.com>
2022-04-02 10:13:05 +0530

    Update floorNum.java

--- floorNum.java
+++ floorNum.java
@@ -2,16 +2,20 @@ package com.fin;
 
 public class floorNum {
     public static void main(String[] args) {
-        int target = 10;
+        int target = 16;
         int[] arr={1,5,9,14,17,20};
 
-        int ans=binarySearch(arr,target);
+        int ans=floor(arr,target);
         System.out.println(ans);
     }
-    public static int binarySearch(int[] arr, int target){
+
+    //return index of smalled num <=target
+    public static int floor(int[] arr, int target){
         int start=0,end=arr.length;
 
         while(start<=end){
+            
+            // start+(end-start)/2; is better than start+end/2 when the array is too big
             int mid = start+((end-start)/2);
 
             if(target<arr[mid]){
